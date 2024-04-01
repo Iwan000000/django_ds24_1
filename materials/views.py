b5497f9e0c4bbd513dc242dfa925fc783c26e091
Ivan <proffesor.09@mail.ru>
2024-04-01 18:03:42 +0300

    26.1 Документирование и безопасность

--- materials/views.py
+++ materials/views.py
@@ -1,66 +1,69 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework import viewsets, generics, status
-from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework import status
+from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from materials.models import Course, Lesson, Subscription
+from materials.models import Course, Subscription
+from materials.models import Lesson
 from materials.paginators import StandardResultsSetPagination
 from materials.serializers import CourseSerializer, LessonSerializer, PaymentSerializer
 from users.models import Payment
 from users.permissions import IsModerator, IsOwner
-from rest_framework import filters
-from django.shortcuts import get_object_or_404
+
 
 class CourseViewSet(viewsets.ModelViewSet):
     """Вью-сет для модели Курса"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     pagination_class = StandardResultsSetPagination
-    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    filter_backends = (DjangoFilterBackend,)
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
     """создание записи"""
 
     serializer_class = LessonSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Lesson.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
 class LessonListAPIView(generics.ListAPIView):
-    """просмотр записи"""
+    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    # permission_classes = [IsAuthenticated]
-
-    # def get_queryset(self):
-    #     if self.request.user.groups.filter(name='moderators').exists():
-    #         return Lesson.objects.all()
-    #     return Lesson.objects.filter(owner=self.request.user)
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('name', 'description')
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='moderators').exists():
+            return Lesson.objects.all()
+        return Lesson.objects.filter(owner=self.request.user)
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     """просмотр конкретной записи"""
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
-    # permission_classes = [IsModerator, IsOwner]
-    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    permission_classes = [IsModerator, IsOwner]
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    search_fields = ('name', 'description')
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     """изменение записи"""
+    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-    # permission_classes = [IsModerator, IsOwner]
+    permission_classes = [IsModerator, IsOwner]
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     """создание записи"""
+    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAdminUser, IsOwner]
 
@@ -68,14 +71,16 @@ class LessonDestroyAPIView(generics.DestroyAPIView):
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,]
     filterset_fields = ['payment_date', 'paid_course', 'paid_lesson', 'payment_method']
     ordering_fields = ['payment_date']
-    # permission_classes = [IsModerator]
+    permission_classes = [IsModerator]
+
 
 class SubscriptionView(APIView):
     """Управление подпиской"""
-    # permission_classes = [IsAuthenticated]  # Убедитесь, что только авторизованные пользователи могут подписаться
+
+    permission_classes = [IsAuthenticated]  #  только авторизованные пользователи могут подписаться
 
     def post(self, request, *args, **kwargs):
         user = request.user
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
+from materials.models import Course, Lesson
 from users.models import User
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Lesson)
